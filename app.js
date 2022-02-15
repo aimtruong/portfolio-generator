169ff54b4209f7664e88587948334afc95dee513
Aimee Truong <aimtruong@ucdavis.edu>
2022-02-14 18:38:25 -0800

    generate html files

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
 
-const printProfileData = profileDataArgs => {
-    for(let i = 0; i < profileDataArgs.length; i+= 1){
-        console.log(profileDataArgs[i]);
-    }
+const profileDataArgs = process.argv.slice(2);
 
-    console.log("==============");
+const [name, github] = profileDataArgs;
 
-    profileDataArgs.forEach((profileItem) => console.log(profileItem));
+    
+fs.writeFile("./index.html", generatePage(name, github), err => {
+    if(err) throw new Error(err);
 
-};
-
-printProfileData(profileDataArgs);
+    console.log("Portfolio complete! Check out index.html to see the output!");
+});
