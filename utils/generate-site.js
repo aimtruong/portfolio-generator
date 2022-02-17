3e7bb5cd0536f20a1ca39517623497bafc7ddfa7
Aimee Truong <aimtruong@ucdavis.edu>
2022-02-17 12:50:33 -0800

    add promises to create copied files and create files

---
+++ utils/generate-site.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile("./dist/index.html", fileContent, err => {
+            // if error, reject promise and send to catch()
+            if(err){
+                reject(err);
+                return;
+            }
+
+            // if all good, resolve promise and send to then()
+            resolve({
+                ok: true,
+                message: "File created!"
+            });
+        });
+    });
+};
+
+const copyFile = () => {
+    return new Promise((resolve, reject) => {
+        fs.copyFile("./src/style.css", "./dist/style.css", err => {
+            // if error, reject promise and send to catch()
+            if(err){
+                reject(err);
+                return;
+            }
+
+            // if all good, resolve promise and send to then()
+            resolve({
+                ok: true,
+                message: "File copied!"
+            });
+        });
+    });
+}
+
+module.exports = {
+    writeFile: writeFile,
+    copyFile: copyFile
+};
